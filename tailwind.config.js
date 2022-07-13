a01de8ab1ddb7995396e9053f9baa1e44bb518c7
Simon Mollet <mollet.simon.pro@gmail.com>
2022-07-13 23:41:30 +0200

    import tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        darkInput: "#2B3945",
+        darkBackground: "#202C37",
+        lightText: "#111517",
+        ligthInput: "#858585",
+        lightBackground: '#FAFAFA',
+        darkText: '#FFFFFF'
+      },
+    },
+  },
+  plugins: [],
+  darkMode: "class",
+};
