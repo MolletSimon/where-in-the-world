92528a8c8af4e53f9ce5dbf2a0f3b4fc49990a8a
Simon Mollet <mollet.simon.pro@gmail.com>
2022-07-15 02:16:49 +0200

    bit of refacto

--- src/screens/detail.jsx
+++ src/screens/detail.jsx
@@ -1,6 +1,40 @@
 import InfosDetails from "../components/Details/infosdetails";
 import BorderCountries from "../components/Details/borderCountries";
 
+function ButtonBack(props) {
+  return (
+    <button
+      onClick={props.onClickBack}
+      className="p-4 pl-6 pr-6 rounded-md shadow-md m-10 font-Nunito font-semibold 
+        flex items-center w-40 justify-center dark:bg-darkInput dark:text-white"
+    >
+      <span className="material-symbols-outlined">arrow_back_ios</span>
+      Back
+    </button>
+  );
+}
+
+function Infos(props) {
+  return (
+    <div className="grid md:grid-cols-2 mt-20">
+      <div className="md:ml-32 flex justify-center md:justify-start">
+        <img
+          src={props.countryDetail.flags.png}
+          className="w-1/2 md:w-3/4 max-h-72"
+        />
+      </div>
+      <div>
+        <InfosDetails countryDetail={props.countryDetail}></InfosDetails>
+        <BorderCountries
+          countries={props.countries}
+          countryDetail={props.countryDetail}
+          setCountryDetail={props.setCountryDetail}
+        ></BorderCountries>
+      </div>
+    </div>
+  );
+}
+
 function Detail({ countryDetail, setDetail, countries, setCountryDetail }) {
   const onClickBack = () => {
     setDetail(false);
@@ -8,36 +42,12 @@ function Detail({ countryDetail, setDetail, countries, setCountryDetail }) {
 
   return (
     <div>
-      <button
-        onClick={onClickBack}
-        className="p-4 pl-6 pr-6 rounded-md shadow-md m-10 font-Nunito font-semibold 
-        flex items-center w-40 justify-center dark:bg-darkInput dark:text-white"
-      >
-        <span className="material-symbols-outlined">arrow_back_ios</span>
-        Back
-      </button>
-      <div className="grid md:grid-cols-2 mt-20">
-        <div className="md:ml-32 flex justify-center md:justify-start">
-          <img
-            src={countryDetail.flags.png}
-            className="w-1/2 md:w-3/4 max-h-64"
-          />
-        </div>
-        <div>
-          <h1
-            className="text-start text-4xl font-Nunito font-extrabold 
-          mt-10 dark:text-darkText md:ml-0 ml-7"
-          >
-            {countryDetail.name.common}
-          </h1>
-          <InfosDetails countryDetail={countryDetail}></InfosDetails>
-          <BorderCountries
-            countries={countries}
-            countryDetail={countryDetail}
-            setCountryDetail={setCountryDetail}
-          ></BorderCountries>
-        </div>
-      </div>
+      <ButtonBack onClickBack={onClickBack}></ButtonBack>
+      <Infos
+        countryDetail={countryDetail}
+        countries={countries}
+        setCountryDetail={setCountryDetail}
+      ></Infos>
     </div>
   );
 }
